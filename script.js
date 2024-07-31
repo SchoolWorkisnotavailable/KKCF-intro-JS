e69e7f23e9e6aaf27f7d22154030d4a32a08ebee
Elvin German <ElvinG13@nycstudents.net>
2024-07-31 16:56:21 +0000

    added alert

--- script.js
+++ script.js
@@ -1,36 +1,46 @@
 //console log text, expression
 console.log("Hello")
 console.log(9+10)
+
 // Create a variable to hold your name as a string
 let Name = "Egghead"
 console.log(Name)
+
 //Concatenate your name variable into a message and log it to the console.
 //Example: “Hello, Zach!”
 console.log("Hello," + Name)
+
 //console log your name variable
 console.log(Name)
+
 //Re-assign the value of your name variable below its declaration and console log it again
 Name = "Eggman"
 console.log(Name)
+
 //practice math operators
 console.log(9*2)
 console.log(10/2)
 console.log(6-2)
 console.log(8+2)
+
 // boolean - true or false
 let bad = false
 console.log(!bad)
+
 //create null and undefined
+
 let meh
 console.log(meh)
+
 //mix data types
 console.log(4+"20")
 let funny = "420"
 funny = Number(funny)
+
 //string methods
 
 //link up html file and create prompt and save value
-
+alert("Hi")
 //access the DOM
 
 
